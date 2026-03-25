f231803d6e98db36506556f1759a64948756d944
AyoubDjerboua <ayoub.djerboua26@gmail.com>
2026-03-25 09:47:28 +0100

    Modification du updateQuality pour incorporer Les Pouvoirs Magiques

--- src/main/java/com/magasin/Magasin.java
+++ src/main/java/com/magasin/Magasin.java
@@ -7,28 +7,44 @@ class Magasin {
         this.items = items;
     }
 
-    public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Comté")
-                    && !items[i].name.equals("Pass VIP Concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Kryptonite")) {
+    public void updateQuality()
+    {
+        for (int i = 0; i < items.length; i++)
+        {
+            if (!items[i].name.equals("Comté") && !items[i].name.equals("Pass VIP Concert"))
+            {
+                if (items[i].quality > 0)
+                {
+                    if (!items[i].name.equals("Kryptonite"))
+                    {
                         items[i].quality = items[i].quality - 1;
                     }
+                    else if(items[i].name.equals("Pouvoir Magiques"))
+                    {
+                        items[i].quality = items[i].quality - 2;
+                    }
                 }
-            } else {
-                if (items[i].quality < 50) {
+            }
+            else
+            {
+                if (items[i].quality < 50)
+                {
                     items[i].quality = items[i].quality + 1;
 
-                    if (items[i].name.equals("Pass VIP Concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
+                    if (items[i].name.equals("Pass VIP Concert"))
+                    {
+                        if (items[i].sellIn < 11)
+                        {
+                            if (items[i].quality < 50)
+                            {
                                 items[i].quality = items[i].quality + 1;
                             }
                         }
 
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
+                        if (items[i].sellIn < 6)
+                        {
+                            if (items[i].quality < 50)
+                            {
                                 items[i].quality = items[i].quality + 1;
                             }
                         }
@@ -36,23 +52,34 @@ class Magasin {
                 }
             }
 
-            if (!items[i].name.equals("Kryptonite")) {
+            if (!items[i].name.equals("Kryptonite"))
+            {
                 items[i].sellIn = items[i].sellIn - 1;
             }
 
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Comté")) {
-                    if (!items[i].name.equals("Pass VIP Concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Kryptonite")) {
+            if (items[i].sellIn < 0)
+            {
+                if (!items[i].name.equals("Comté"))
+                {
+                    if (!items[i].name.equals("Pass VIP Concert"))
+                    {
+                        if (items[i].quality > 0)
+                        {
+                            if (!items[i].name.equals("Kryptonite"))
+                            {
                                 items[i].quality = items[i].quality - 1;
                             }
                         }
-                    } else {
+                    }
+                    else
+                    {
                         items[i].quality = items[i].quality - items[i].quality;
                     }
-                } else {
-                    if (items[i].quality < 50) {
+                }
+                else
+                {
+                    if (items[i].quality < 50)
+                    {
                         items[i].quality = items[i].quality + 1;
                     }
                 }
